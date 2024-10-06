34639b14d043716eb1b4b2b174e1831779b1b25c
Raiyan27 <abdullahalraiyan4@gmail.com>
2024-10-06 10:36:48 +0600

    loading spinner working

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 const loadAllPets = async()=>{
     const response = await fetch('https://openapi.programming-hero.com/api/peddy/pets');
     const data = await response.json();
+    const postContainer = document.getElementById('post-container');
+    postContainer.innerHTML = ``;
+
     setTimeout(() => {
         displayAllPets(data.pets);
         const loadingSpinner = document.getElementById('loading');
@@ -16,18 +19,29 @@ const loadAllCategory = async () => {
     const response = await fetch('https://openapi.programming-hero.com/api/peddy/categories');
     const data = await response.json();
     displayAllCategory(data.categories);
-    console.log(data.categories);
 };
 const loadByCategory = async(category) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`);
     const pet = await response.json();
-    displayAllPets(pet.data);
+    
+    const postContainer = document.getElementById('post-container');
+    postContainer.innerHTML = "";
 
+    setTimeout(() => {
+        displayAllPets(pet.data);
+        const loadingSpinner = document.getElementById('loading');
+
+        if (loadingSpinner) {
+            loadingSpinner.classList.add('hidden');
+        }
+    }, 2000);
+    const loadingSpinner = document.getElementById('loading');
+    loadingSpinner.classList.remove('hidden');
 }
 
 const displayAllCategory = (categories) => {
     const categoryContainer = document.getElementById('category-container');
-    categoryContainer.innerHTML = ''; // Clear previous categories
+    categoryContainer.innerHTML = ''; 
 
     categories.forEach(category => {
         const btn = document.createElement('div');
@@ -47,40 +61,48 @@ const displayAllCategory = (categories) => {
 const displayAllPets = async (pets) => {
     const postContainer = document.getElementById('post-container');
     postContainer.innerHTML = "";
-    setTimeout(() => {
-        pets.forEach(pets => {
-            const div = document.createElement('div');
-            div.innerHTML = `
-                <div class="shadow-xl border rounded-xl">
-                    <figure class="px-2 pt-2 w-full h-56">
-                        <img
-                        src="${pets.image}"
-                        alt="Pet Image"
-                        class="rounded-xl object-cover w-full h-full" />
-                    </figure>
-                    <div class="card-body">
-                        <h2 class="card-title text-2xl font-bold truncate">${pets.pet_name ? pets.pet_name : 'Information Unavailable'}</h2>
-                        <ul class="flex flex-col">
-                        <li>Breed: ${pets.breed ? pets.breed : 'Information Unavailable'}</li>
-                        <li>Birth: ${pets.date_of_birth ? pets.date_of_birth : 'Information Unavailable'}</li>
-                        <li>Gender: ${pets.gender ? pets.gender : 'Information Unavailable'}</li>
-                        <li>Price: $${pets.price ? pets.price : 'Information Unavailable'}</li>
-                        </ul>
-                        <hr>
-                        <div class="flex gap-4 justify-center">
-                        <button onclick="likedPets('${pets.image}')" class="btn text-black bg-white border-base-100 hover:bg-pink-200">
-                            <img src="./images/like.png" alt="Like" />
-                        </button>
-                        <button class="btn text-black bg-white border-base-100 hover:bg-pink-200">Adopt</button>
-                        <button onclick="showDetail('${pets.id}')" class="btn text-black bg-white border-base-100 hover:bg-pink-200">Details</button>
-                        </div>
+    // handle empty return
+    if(pets.length === 0){
+        postContainer.innerHTML = `  
+        <div class="text-center bg-orange-100 col-span-4 rounded-lg">
+            <h1 class='text-3xl'>No Information</h1>
+        </div>`;
+        return; 
+    }
+
+
+    pets.forEach(pets => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="shadow-xl border rounded-xl">
+                <figure class="px-2 pt-2 w-full h-56">
+                    <img
+                    src="${pets.image}"
+                    alt="Pet Image"
+                    class="rounded-xl object-cover w-full h-full" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title text-2xl font-bold truncate">${pets.pet_name ? pets.pet_name : 'Information Unavailable'}</h2>
+                    <ul class="flex flex-col">
+                    <li>Breed: ${pets.breed ? pets.breed : 'Information Unavailable'}</li>
+                    <li>Birth: ${pets.date_of_birth ? pets.date_of_birth : 'Information Unavailable'}</li>
+                    <li>Gender: ${pets.gender ? pets.gender : 'Information Unavailable'}</li>
+                    <li>Price: $${pets.price ? pets.price : 'Information Unavailable'}</li>
+                    </ul>
+                    <hr>
+                    <div class="flex gap-4 justify-center">
+                    <button onclick="likedPets('${pets.image}')" class="btn text-black bg-white border-base-100 hover:bg-pink-200">
+                        <img src="./images/like.png" alt="Like" />
+                    </button>
+                    <button class="btn text-black bg-white border-base-100 hover:bg-pink-200">Adopt</button>
+                    <button onclick="showDetail('${pets.id}')" class="btn text-black bg-white border-base-100 hover:bg-pink-200">Details</button>
                     </div>
                 </div>
-                `;
-    
-              postContainer.appendChild(div);
-        });
-    }, 1);
+            </div>
+            `;
+
+          postContainer.appendChild(div);
+    });
 
 
     
